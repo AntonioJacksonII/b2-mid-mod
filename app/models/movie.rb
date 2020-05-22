5b504568e32ad48b75ef57b5539f39bf59e569f7
Antonio Jackson <antoniojackson@gmail.com>
2020-05-22 10:35:29 -0600

    Passing tests for User Story 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def stars
+    actors.order(:age).pluck(:name).join(", ")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/studios', to: 'studios#index'
+  get '/movies/:movie_id', to: 'movies#show'
 end
