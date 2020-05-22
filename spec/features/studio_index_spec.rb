f78b7b3ad20a9a4024b245370d331b5b7798876b
Antonio Jackson <antoniojackson@gmail.com>
2020-05-22 09:56:39 -0600

    Adding test for User Story 1

---
+++ spec/features/studio_index_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe "Studio Index Page", type: :feature do
+  before :each do
+    @cbs = Studio.create(name: "CBS Studios", location: "Los Angeles")
+    @abc = Studio.create(name: "ABC Studios", location: "Glendale")
+    @columbia = Studio.create(name: "Columbia Pictures", location: "New York")
+
+    @apes = @cbs.movies.create!(title: "Planet of the Apes", year: "2000", genre: "Science Fiction")
+    @mean = @cbs.movies.create!(title: "Mean Girls", year: "2010", genre: "Comedy")
+    @godfather = @columbia.movies.create!(title: "The Godfather", year: "1980", genre: "Action")
+    @prada = @abc.movies.create!(title: "The Devil Wears Prada", year: "2003", genre: "Fashion")
+
+    visit "/studios"
+  end
+
+  it 'it shows a list of all movie studies with their movies' do
+    expect(page).to have_content("CBS Studios")
+    expect(page).to have_content("Movies: Planet of the Apes, Mean Girls")
+
+    expect(page).to have_content("ABC Studios")
+    expect(page).to have_content("Movies: The Devil Wears Prada")
+
+    expect(page).to have_content("Columbia Pictures")
+    expect(page).to have_content("Movies: The Godfather")
+  end
+end
+
+# As a visitor,
+# When I visit the studio index page
+# I see a list of all of the movie studios
+# And underneath each studio, I see the names of all of its movies.
