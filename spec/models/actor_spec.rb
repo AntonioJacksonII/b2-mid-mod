a1033e7859565d1665a69931733dd4ccf21178c9
Antonio Jackson <antoniojackson@gmail.com>
2020-05-22 09:33:49 -0600

    Updating actor & movie relationship tests

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Actor, type: :model do
   describe 'relationships' do
-    it {should have_many :movies}
+    it {should have_many :movie_actors}
+    it {should have_many(:movies).through(:movie_actors)}
   end
 end
